bacf89631b7ad97f6ac90cf24507646d140324a1
Sequajaa <sequaja.marco@gmail.com>
2023-04-03 20:42:07 +0200

    changed dark/light mode icon

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useTheme } from 'next-themes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const ThemeSwitcher: React.FC = () => {
-  const { theme, setTheme } = useTheme();
+const { theme, setTheme } = useTheme();
 
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
+const toggleTheme = () => {
+setTheme(theme === 'light' ? 'dark' : 'light');
+};
 
-  return (
-    <button
-      className="focus:outline-none text-gray-700 dark:text-gray-300 hover:text-indigo-600"
-      onClick={toggleTheme}
-    >
-      {theme === 'light' ? '🌙' : '☀️'}
-    </button>
-  );
+return (
+<button
+   className="focus:outline-none text-white dark:text-gray-300 hover:text-indigo-600"
+   onClick={toggleTheme}
+ >
+{theme === 'light' ? (
+<FontAwesomeIcon icon={faMoon} />
+) : (
+<FontAwesomeIcon icon={faSun} />
+)}
+</button>
+);
 };
 
 export default ThemeSwitcher;
